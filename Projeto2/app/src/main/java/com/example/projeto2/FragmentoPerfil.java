d434c725cafcfcdf02ee0435b239f96090d8e68c
Mariano-Lucas <erickprovesi@gmail.com>
2022-09-15 16:54:35 -0300

    Att TCC
    Perfil quase finalizado, falta só os botões de alterações.

--- Projeto2/app/src/main/java/com/example/projeto2/FragmentoPerfil.java
+++ Projeto2/app/src/main/java/com/example/projeto2/FragmentoPerfil.java
@@ -6,22 +6,41 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Layout;
+import android.text.method.KeyListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.ItemTouchUIUtil;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class FragmentoPerfil extends Fragment {
 
-    private  Dialog EditEmail;
+    private KeyListener listener;
+
+    private Dialog EditEmail, EditPassword, DeleteAccount, Warning;
+    private String userID;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Nullable
     @Override
@@ -31,19 +50,89 @@ public class FragmentoPerfil extends Fragment {
         Button btnEditEmail = view.findViewById(R.id.btnEditEmail);
         Button btnEditPassword = view.findViewById(R.id.btnEditPassword);
         Button btnDeleteAccount = view.findViewById(R.id.btnDeleteAccount);
+        ImageView imgProfilePic = view.findViewById(R.id.imgProfilePic);
+        ImageView imgEditName = view.findViewById(R.id.imgEditName);
+        TextView txtEmailProfile = view.findViewById(R.id.txtEmailProfile);
+        EditText edtProfileName = view.findViewById(R.id.edtProfileName);
+        ImageView imgProfileCheck = view.findViewById(R.id.imgProfileCheck);
+
+        listener = edtProfileName.getKeyListener();
+
+        edtProfileName.setKeyListener(null);
+
 
         EditEmail = new Dialog(getActivity());
+        EditPassword = new Dialog(getActivity());
+        DeleteAccount = new Dialog(getActivity());
+        Warning = new Dialog(getActivity());
 
-        btnEditEmail.setOnClickListener(new View.OnClickListener() {
+        userID = mAuth.getCurrentUser().getUid();
+
+        txtEmailProfile.setText(mAuth.getCurrentUser().getEmail());
+
+        db.collection("Usuario").whereEqualTo("idUsuario", userID).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onClick(View view) {
-                showEmailPopup(view);
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                       edtProfileName.setText(document.getString("nomeUsuario"));
+
+                    }
+
+                }
             }
         });
 
 
-        return view;
+        btnEditEmail.setOnClickListener(view13 -> showEmailPopup(view13));
 
+        btnEditPassword.setOnClickListener(view12 -> showPasswordPopup(view12));
+
+        btnDeleteAccount.setOnClickListener(view1 -> showDeletePopup(view1));
+
+        imgEditName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtProfileName.setKeyListener(listener);
+                imgEditName.setVisibility(View.INVISIBLE);
+                imgProfileCheck.setVisibility(View.VISIBLE);
+
+                imgProfileCheck.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (edtProfileName.getText().toString().isEmpty()){
+                            imgProfileCheck.setVisibility(View.INVISIBLE);
+                            edtProfileName.setError("Não deixe em branco!");
+
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    edtProfileName.setError(null);
+                                    imgProfileCheck.setVisibility(View.VISIBLE);
+
+
+                                }
+                            },1000);
+
+
+                        }else {
+                            db.collection("Usuario").document(userID).update("nomeUsuario", edtProfileName.getText().toString());
+                            imgProfileCheck.setVisibility(View.INVISIBLE);
+                            imgEditName.setVisibility(View.VISIBLE);
+                            edtProfileName.setKeyListener(null);
+                        }
+                    }
+                });
+
+
+
+
+            }
+        });
+
+        return view;
 
     }
     public void showEmailPopup(View view){
@@ -63,4 +152,58 @@ public class FragmentoPerfil extends Fragment {
         EditEmail.show();
     }
 
+    public void showPasswordPopup(View view) {
+        Button btnModifyPassword;
+        EditText edtCurrentPassword, edtNewPassword, edtConfirmNewPassword;
+
+        EditPassword.setContentView(R.layout.popup_alterarsenha);
+
+        btnModifyPassword = EditPassword.findViewById(R.id.btnModifyPassword);
+        edtCurrentPassword = EditPassword.findViewById(R.id.edtCurrentPassword);
+        edtNewPassword = EditPassword.findViewById(R.id.edtNewPassword);
+        edtConfirmNewPassword = EditPassword.findViewById(R.id.edtConfirmNewPassword);
+
+        EditPassword.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        EditPassword.show();
+    }
+
+    public void showDeletePopup(View view) {
+        EditText edtEmail, edtPassword, edtConfirmPassword;
+        Button btnDeleteAccountPopUp;
+
+        DeleteAccount.setContentView(R.layout.popup_excluirconta);
+
+        edtEmail = DeleteAccount.findViewById(R.id.edtEmail);
+        edtPassword = DeleteAccount.findViewById(R.id.edtPassword);
+        edtConfirmPassword = DeleteAccount.findViewById(R.id.edtConfirmPassword);
+        btnDeleteAccountPopUp = DeleteAccount.findViewById(R.id.btnDeleteAccountPopUp);
+
+        btnDeleteAccountPopUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DeleteAccount.dismiss();
+                showWarningPopup();
+            }
+        });
+
+        DeleteAccount.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        DeleteAccount.show();
+
+    }
+
+    public void showWarningPopup() {
+        Button btnReturn, btnConfirm;
+
+        Warning.setContentView(R.layout.popup_aviso);
+
+        btnReturn = Warning.findViewById(R.id.btnReturn);
+        btnConfirm = Warning.findViewById(R.id.btnConfirm);
+
+        Warning.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        Warning.show();
+
+    }
+
 }
+
+
--- Projeto2/app/src/main/java/com/example/projeto2/FragmentoProduto.java
+++ Projeto2/app/src/main/java/com/example/projeto2/FragmentoProduto.java
@@ -1,21 +1,115 @@
 package com.example.projeto2;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class FragmentoProduto extends Fragment {
+
+    private Dialog addProduct;
+    public static int voltou = 0;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String userID;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return (ViewGroup)inflater.inflate(R.layout.activity_principal,container,false);
+        View view = inflater.inflate(R.layout.activity_principal, container, false);
+
+        FloatingActionButton btnAddProduct = view.findViewById(R.id.btnAddProduct);
+
+        addProduct = new Dialog(getActivity());
+
+        voltou = 0;
+
+        btnAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopup(view);
+            }
+        });
+
+        return view;
+
+    }
+
+    public void showPopup(View view) {
+
+        Button btnSaveProduct;
+        EditText edtNameProduct;
+        addProduct.setContentView(R.layout.popup_adicionarproduto);
+
+        edtNameProduct = addProduct.findViewById(R.id.edtNameProduct);
+        btnSaveProduct = addProduct.findViewById(R.id.btnSaveProduct);
+
+        btnSaveProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtNameProduct.getText().toString().isEmpty()) {
+                    edtNameProduct.setError("Não deixe o campo vazio");
+
+                }else {
+
+                    userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
 
+                    Map<String, Object> product = new HashMap<>();
+                    product.put("capacidadeBotijao", 0);
+                    product.put("consumoQueimadorGas",0);
+                    product.put("descricaoProduto",0);
+                    product.put("fotoProduto",0);
+                    product.put("idUsuario", userID);
+                    product.put("idProduto",0);
+                    product.put("kmLitroVeiculo",0);
+                    product.put("kmRodado",0);
+                    product.put("margemLucro",0);
+                    product.put("nomeProduto",edtNameProduct.getText().toString());
+                    product.put("outroCusto",0);
+                    product.put("precoBotijao",0);
+                    product.put("precoCombustivel",0);
+                    product.put("tempoUsoEnergia",0);
+                    product.put("tempoUsoGas",0);
+                    product.put("valorKWH",0);
+
+                    DocumentReference documentReference = db.collection("Produto").document(UUID.randomUUID().toString());
+                    documentReference.set(product).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d("db", "Sucesso ao salvar os dados");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("db_error" ,"Erro ao salvar os dados" + e.toString());
+                                }
+                            });
+                }
+            }
+        });
+        addProduct.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        addProduct.show();
     }
 }
